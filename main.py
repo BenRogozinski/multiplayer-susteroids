dda70931cd668d74e99b5493f62ed2bbaa3e5713
Ben Rogozinski <brogodev@gmail.com>
2023-09-01 20:23:06 -0500

    Did stuff

--- main.py
+++ main.py
@@ -27,19 +27,33 @@ def on_player2_button_a_pressed():
     blueCrewmate.set_image(assets.image("""
         blueCrewmate2
     """))
+    music.play(music.melody_playable(music.pew_pew),
+        music.PlaybackMode.IN_BACKGROUND)
     blueLaser = sprites.create(assets.image("""
         blueLaser
     """), SpriteKind.projectile)
     blueLaser.set_position(130, blueCrewmate.y)
     blueLaser.set_flag(SpriteFlag.AUTO_DESTROY, True)
-    blueLaser.set_velocity(-200, 0)
+    blueLaser.set_velocity(-250, 0)
     pause(100)
     blueCrewmate.set_image(assets.image("""
         blueCrewmate1
     """))
+    pause(fireDelay)
 controller.player2.on_button_event(ControllerButton.A,
     ControllerButtonEvent.PRESSED,
     on_player2_button_a_pressed)
+
+def initializeGame():
+    global playersRemaining, gameStarted
+    scene.set_background_image(assets.image("""
+        spaceBackground
+    """))
+    info.set_score(0)
+    playersRemaining = 0
+    gameStarted = 0
+    game.set_game_over_effect(True, effects.none)
+    game.set_game_over_message(True, "GAME OVER!")
 
 def on_player3_connected():
     global yellowCrewmate, playersRemaining, gameStarted
@@ -53,6 +67,20 @@ def on_player3_connected():
     gameStarted = 1
 controller.player3.on_event(ControllerEvent.CONNECTED, on_player3_connected)
 
+def on_on_overlap(sprite2, otherSprite):
+    sprites.destroy(sprite2)
+    sprites.destroy(otherSprite, effects.disintegrate, 50)
+    info.change_score_by(1)
+    pause(20)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
+
+def on_on_overlap2(sprite3, otherSprite2):
+    global playersRemaining
+    sprites.destroy(sprite3, effects.disintegrate, 100)
+    sprites.destroy(otherSprite2, effects.disintegrate, 100)
+    playersRemaining += -1
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
 def on_player2_disconnected():
     sprites.destroy(blueCrewmate)
 controller.player2.on_event(ControllerEvent.DISCONNECTED, on_player2_disconnected)
@@ -62,16 +90,19 @@ def on_player4_button_a_pressed():
     greenCrewmate.set_image(assets.image("""
         greenCrewmate2
     """))
+    music.play(music.melody_playable(music.pew_pew),
+        music.PlaybackMode.IN_BACKGROUND)
     greenLaser = sprites.create(assets.image("""
         greenLaser
     """), SpriteKind.projectile)
     greenLaser.set_position(130, greenCrewmate.y)
     greenLaser.set_flag(SpriteFlag.AUTO_DESTROY, True)
-    greenLaser.set_velocity(-200, 0)
+    greenLaser.set_velocity(-250, 0)
     pause(100)
     greenCrewmate.set_image(assets.image("""
         greenCrewmate1
     """))
+    pause(fireDelay)
 controller.player4.on_button_event(ControllerButton.A,
     ControllerButtonEvent.PRESSED,
     on_player4_button_a_pressed)
@@ -81,16 +112,19 @@ def on_player1_button_a_pressed():
     redCrewmate.set_image(assets.image("""
         redCrewmate2
     """))
+    music.play(music.melody_playable(music.pew_pew),
+        music.PlaybackMode.IN_BACKGROUND)
     redLaser = sprites.create(assets.image("""
         redLaser
     """), SpriteKind.projectile)
     redLaser.set_position(130, redCrewmate.y)
     redLaser.set_flag(SpriteFlag.AUTO_DESTROY, True)
-    redLaser.set_velocity(-200, 0)
+    redLaser.set_velocity(-250, 0)
     pause(100)
     redCrewmate.set_image(assets.image("""
         redCrewmate1
     """))
+    pause(fireDelay)
 controller.player1.on_button_event(ControllerButton.A,
     ControllerButtonEvent.PRESSED,
     on_player1_button_a_pressed)
@@ -100,16 +134,19 @@ def on_player3_button_a_pressed():
     yellowCrewmate.set_image(assets.image("""
         yellowCrewmate2
     """))
+    music.play(music.melody_playable(music.pew_pew),
+        music.PlaybackMode.IN_BACKGROUND)
     yellowLaser = sprites.create(assets.image("""
         yellowLaser
     """), SpriteKind.projectile)
     yellowLaser.set_position(130, yellowCrewmate.y)
     yellowLaser.set_flag(SpriteFlag.AUTO_DESTROY, True)
-    yellowLaser.set_velocity(-200, 0)
+    yellowLaser.set_velocity(-250, 0)
     pause(100)
     yellowCrewmate.set_image(assets.image("""
         yellowCrewmate1
     """))
+    pause(fireDelay)
 controller.player3.on_button_event(ControllerButton.A,
     ControllerButtonEvent.PRESSED,
     on_player3_button_a_pressed)
@@ -143,19 +180,6 @@ def on_player1_connected():
     gameStarted = 1
 controller.player1.on_event(ControllerEvent.CONNECTED, on_player1_connected)
 
-def on_on_overlap(sprite2, otherSprite):
-    sprites.destroy(sprite2)
-    sprites.destroy(otherSprite, effects.disintegrate, 50)
-    info.change_score_by(1)
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
-
-def on_on_overlap2(sprite3, otherSprite2):
-    global playersRemaining
-    sprites.destroy(sprite3, effects.disintegrate, 100)
-    sprites.destroy(otherSprite2, effects.disintegrate, 100)
-    playersRemaining += -1
-sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
-
 asteroidSmall: Sprite = None
 yellowLaser: Sprite = None
 redLaser: Sprite = None
@@ -164,26 +188,23 @@ blueLaser: Sprite = None
 blueCrewmate: Sprite = None
 redCrewmate: Sprite = None
 yellowCrewmate: Sprite = None
-greenCrewmate: Sprite = None
 gameStarted = 0
-scene.set_background_image(assets.image("""
-    spaceBackground
-"""))
-info.set_score(0)
 playersRemaining = 0
-gameStarted = 0
-game.set_game_over_effect(True, effects.none)
-game.set_game_over_message(True, "GAME OVER!")
+greenCrewmate: Sprite = None
+fireDelay = 0
+fireDelay = 100
+gameDifficultyMultiplier = 1
+initializeGame()
 
 def on_update_interval():
     global asteroidSmall
     if playersRemaining == 0 and gameStarted == 1:
         game.game_over(True)
-    for index in range(Math.round(playersRemaining / 2)):
+    for index in range(Math.round(playersRemaining * gameDifficultyMultiplier)):
         asteroidSmall = sprites.create(assets.image("""
             asteroidSmall
         """), SpriteKind.enemy)
         asteroidSmall.set_flag(SpriteFlag.AUTO_DESTROY, True)
         asteroidSmall.set_position(0, randint(8, 112))
         asteroidSmall.set_velocity(50, 0)
-game.on_update_interval(500, on_update_interval)
+game.on_update_interval(750, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,19 +1,36 @@
-controller.player4.onEvent(ControllerEvent.Connected, function () {
-    greenCrewmate = sprites.create(assets.image`greenCrewmate1`, SpriteKind.Player)
-    greenCrewmate.setPosition(146, 105)
-    greenCrewmate.setStayInScreen(true)
-    controller.player4.moveSprite(greenCrewmate, 0, 100)
-    playersRemaining += 1
-    gameStarted = 1
+namespace SpriteKind {
+    export const deadPlayer = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite2, otherSprite) {
+    sprites.destroy(sprite2)
+    sprites.destroy(otherSprite, effects.disintegrate, 50)
+    info.changeScoreBy(1)
+    pause(20)
 })
-controller.player3.onEvent(ControllerEvent.Disconnected, function () {
-    sprites.destroy(yellowCrewmate)
+controller.player2.onEvent(ControllerEvent.Disconnected, function () {
+    sprites.destroy(blueCrewmate)
 })
-controller.player4.onEvent(ControllerEvent.Disconnected, function () {
-    sprites.destroy(greenCrewmate)
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    redCrewmate.setImage(assets.image`redCrewmate2`)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
+    redLaser = sprites.create(assets.image`redLaser`, SpriteKind.Projectile)
+    redLaser.setPosition(130, redCrewmate.y)
+    redLaser.setFlag(SpriteFlag.AutoDestroy, true)
+    redLaser.setVelocity(-250, 0)
+    pause(100)
+    redCrewmate.setImage(assets.image`redCrewmate1`)
+    pause(fireDelay)
 })
 controller.player1.onEvent(ControllerEvent.Disconnected, function () {
     sprites.destroy(redCrewmate)
+})
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    blueCrewmate = sprites.create(assets.image`blueCrewmate1`, SpriteKind.Player)
+    blueCrewmate.setPosition(146, 45)
+    blueCrewmate.setStayInScreen(true)
+    controller.player2.moveSprite(blueCrewmate, 0, 100)
+    playersRemaining += 1
+    gameStarted = 1
 })
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     blueCrewmate.setImage(assets.image`blueCrewmate2`)
@@ -21,7 +38,7 @@ controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Press
     blueLaser = sprites.create(assets.image`blueLaser`, SpriteKind.Projectile)
     blueLaser.setPosition(130, blueCrewmate.y)
     blueLaser.setFlag(SpriteFlag.AutoDestroy, true)
-    blueLaser.setVelocity(-200, 0)
+    blueLaser.setVelocity(-250, 0)
     pause(100)
     blueCrewmate.setImage(assets.image`blueCrewmate1`)
     pause(fireDelay)
@@ -34,6 +51,33 @@ function initializeGame () {
     game.setGameOverEffect(true, effects.none)
     game.setGameOverMessage(true, "GAME OVER!")
 }
+controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    yellowCrewmate.setImage(assets.image`yellowCrewmate2`)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
+    yellowLaser = sprites.create(assets.image`yellowLaser`, SpriteKind.Projectile)
+    yellowLaser.setPosition(130, yellowCrewmate.y)
+    yellowLaser.setFlag(SpriteFlag.AutoDestroy, true)
+    yellowLaser.setVelocity(-250, 0)
+    pause(100)
+    yellowCrewmate.setImage(assets.image`yellowCrewmate1`)
+    pause(fireDelay)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite3, otherSprite2) {
+    sprites.destroy(otherSprite2, effects.disintegrate, 100)
+    sprites.destroy(sprite3, effects.disintegrate, 100)
+    playersRemaining += -1
+})
+controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    greenCrewmate.setImage(assets.image`greenCrewmate2`)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
+    greenLaser = sprites.create(assets.image`greenLaser`, SpriteKind.Projectile)
+    greenLaser.setPosition(130, greenCrewmate.y)
+    greenLaser.setFlag(SpriteFlag.AutoDestroy, true)
+    greenLaser.setVelocity(-250, 0)
+    pause(100)
+    greenCrewmate.setImage(assets.image`greenCrewmate1`)
+    pause(fireDelay)
+})
 controller.player3.onEvent(ControllerEvent.Connected, function () {
     yellowCrewmate = sprites.create(assets.image`yellowCrewmate1`, SpriteKind.Player)
     yellowCrewmate.setPosition(146, 75)
@@ -42,54 +86,21 @@ controller.player3.onEvent(ControllerEvent.Connected, function () {
     playersRemaining += 1
     gameStarted = 1
 })
-controller.player2.onEvent(ControllerEvent.Disconnected, function () {
-    sprites.destroy(blueCrewmate)
-})
-controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    greenCrewmate.setImage(assets.image`greenCrewmate2`)
-    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
-    greenLaser = sprites.create(assets.image`greenLaser`, SpriteKind.Projectile)
-    greenLaser.setPosition(130, greenCrewmate.y)
-    greenLaser.setFlag(SpriteFlag.AutoDestroy, true)
-    greenLaser.setVelocity(-200, 0)
-    pause(100)
-    greenCrewmate.setImage(assets.image`greenCrewmate1`)
-    pause(fireDelay)
-})
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    redCrewmate.setImage(assets.image`redCrewmate2`)
-    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
-    redLaser = sprites.create(assets.image`redLaser`, SpriteKind.Projectile)
-    redLaser.setPosition(130, redCrewmate.y)
-    redLaser.setFlag(SpriteFlag.AutoDestroy, true)
-    redLaser.setVelocity(-200, 0)
-    pause(100)
-    redCrewmate.setImage(assets.image`redCrewmate1`)
-    pause(fireDelay)
-})
-controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    yellowCrewmate.setImage(assets.image`yellowCrewmate2`)
-    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
-    yellowLaser = sprites.create(assets.image`yellowLaser`, SpriteKind.Projectile)
-    yellowLaser.setPosition(130, yellowCrewmate.y)
-    yellowLaser.setFlag(SpriteFlag.AutoDestroy, true)
-    yellowLaser.setVelocity(-200, 0)
-    pause(100)
-    yellowCrewmate.setImage(assets.image`yellowCrewmate1`)
-    pause(fireDelay)
-})
-controller.player2.onEvent(ControllerEvent.Connected, function () {
-    blueCrewmate = sprites.create(assets.image`blueCrewmate1`, SpriteKind.Player)
-    blueCrewmate.setPosition(146, 45)
-    blueCrewmate.setStayInScreen(true)
-    controller.player2.moveSprite(blueCrewmate, 0, 100)
-    playersRemaining += 1
-    gameStarted = 1
+controller.player4.onEvent(ControllerEvent.Disconnected, function () {
+    sprites.destroy(greenCrewmate)
 })
 sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
     if (sprite.x >= 160) {
         game.gameOver(true)
     }
+})
+controller.player4.onEvent(ControllerEvent.Connected, function () {
+    greenCrewmate = sprites.create(assets.image`greenCrewmate1`, SpriteKind.Player)
+    greenCrewmate.setPosition(146, 105)
+    greenCrewmate.setStayInScreen(true)
+    controller.player4.moveSprite(greenCrewmate, 0, 100)
+    playersRemaining += 1
+    gameStarted = 1
 })
 controller.player1.onEvent(ControllerEvent.Connected, function () {
     redCrewmate = sprites.create(assets.image`redCrewmate1`, SpriteKind.Player)
@@ -99,35 +110,29 @@ controller.player1.onEvent(ControllerEvent.Connected, function () {
     playersRemaining += 1
     gameStarted = 1
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    sprites.destroy(otherSprite, effects.disintegrate, 50)
-    info.changeScoreBy(1)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(sprite, effects.disintegrate, 100)
-    sprites.destroy(otherSprite, effects.disintegrate, 100)
-    playersRemaining += -1
+controller.player3.onEvent(ControllerEvent.Disconnected, function () {
+    sprites.destroy(yellowCrewmate)
 })
 let asteroidSmall: Sprite = null
-let yellowLaser: Sprite = null
-let redLaser: Sprite = null
 let greenLaser: Sprite = null
-let blueLaser: Sprite = null
-let blueCrewmate: Sprite = null
-let redCrewmate: Sprite = null
+let greenCrewmate: Sprite = null
+let yellowLaser: Sprite = null
 let yellowCrewmate: Sprite = null
+let blueLaser: Sprite = null
 let gameStarted = 0
 let playersRemaining = 0
-let greenCrewmate: Sprite = null
+let redLaser: Sprite = null
+let redCrewmate: Sprite = null
+let blueCrewmate: Sprite = null
 let fireDelay = 0
 fireDelay = 100
+let gameDifficultyMultiplier = 1
 initializeGame()
-game.onUpdateInterval(500, function () {
+game.onUpdateInterval(750, function () {
     if (playersRemaining == 0 && gameStarted == 1) {
         game.gameOver(true)
     }
-    for (let index = 0; index < Math.round(playersRemaining / 2); index++) {
+    for (let index = 0; index < Math.round(playersRemaining * gameDifficultyMultiplier); index++) {
         asteroidSmall = sprites.create(assets.image`asteroidSmall`, SpriteKind.Enemy)
         asteroidSmall.setFlag(SpriteFlag.AutoDestroy, true)
         asteroidSmall.setPosition(0, randint(8, 112))
